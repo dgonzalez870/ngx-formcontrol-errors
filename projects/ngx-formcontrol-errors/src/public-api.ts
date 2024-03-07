d46db7e357818a7a86130da03612415051544095
Dennys González <dgonzalez870@yahoo.com>
2024-03-07 19:43:49 -0400

    refactor: Expose the correct directive library APIs

--- projects/ngx-formcontrol-errors/src/public-api.ts
+++ projects/ngx-formcontrol-errors/src/public-api.ts
@@ -2,5 +2,4 @@
  * Public API Surface of ngx-formcontrol-errors
  */
 
-export * from './lib/ngx-formcontrol-errors.service';
-export * from './lib/ngx-formcontrol-errors.component';
+export * from './lib/formcontrol-errors.directive';
