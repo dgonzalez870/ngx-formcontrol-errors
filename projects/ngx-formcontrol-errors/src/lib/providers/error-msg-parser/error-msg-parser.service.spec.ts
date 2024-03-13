a1e2fa21f36ddc0e4eaeb4d65f507cac13524dbe
Dennys González <dgonzalez870@yahoo.com>
2024-03-13 10:14:54 -0400

    test: add test cases to `ErrorMsgParserService`

--- projects/ngx-formcontrol-errors/src/lib/providers/error-msg-parser/error-msg-parser.service.spec.ts
+++ projects/ngx-formcontrol-errors/src/lib/providers/error-msg-parser/error-msg-parser.service.spec.ts
@@ -13,4 +13,16 @@ describe('ErrorMsgParserService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should parse error message for the `required` validation error', () => {
+    const result = service.parse({ required: true });
+    expect(result).toEqual('This field is required');
+  });
+
+  it('should parse error message for the `min` validation error', () => {
+    const result = service.parse({
+      min: { min: 10, actual: 8 },
+    });
+    expect(result).toEqual('The minimun allowed values is 10');
+  });
 });
