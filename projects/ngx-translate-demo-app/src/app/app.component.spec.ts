99505b43e18ac8ddfd5a8bc188c6867bb6563613
Dennys González <dgonzalez870@yahoo.com>
2024-03-15 20:48:30 -0400

    test: fix broken test case

--- projects/ngx-translate-demo-app/src/app/app.component.spec.ts
+++ projects/ngx-translate-demo-app/src/app/app.component.spec.ts
@@ -1,11 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Observable } from 'rxjs';
+
+import {
+  TranslateLoader,
+  TranslateModule,
+} from '@ngx-translate/core';
+
 import { AppComponent } from './app.component';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const translations: any = {
+  TITLE: 'Mocked Title',
+  APP_DESCRIPTION: 'Mocked Description',
+};
+
+class MockTranslateLoader extends TranslateLoader {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  override getTranslation(lang: string): Observable<any> {
+    return translations;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  get(lang: string): any {
+    return translations;
+  }
+}
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: MockTranslateLoader,
+          },
+        }),
+      ],
     }).compileComponents();
   });
 
@@ -25,6 +56,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('ngx-translate-demo-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'ngx-translate-demo-app'
+    );
   });
 });
