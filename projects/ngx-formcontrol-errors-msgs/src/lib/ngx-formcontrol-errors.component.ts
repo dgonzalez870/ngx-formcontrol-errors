541f31fade1437225feb10c07497d1a83f2b944e
Dennys González <dgonzalez870@yahoo.com>
2024-03-18 17:34:33 -0400

    feat(ngx-formcontrol-errors-msgs): allow HTML tags in messages

--- projects/ngx-formcontrol-errors-msgs/src/lib/ngx-formcontrol-errors.component.ts
+++ projects/ngx-formcontrol-errors-msgs/src/lib/ngx-formcontrol-errors.component.ts
@@ -1,14 +1,19 @@
 import {
   Component,
+  ElementRef,
   Input,
 } from '@angular/core';
 
 @Component({
   selector: 'ngx-formcontrol-errors',
   standalone: true,
-  template: `{{ message }}`,
+  template: ``,
 })
 export class NgxFormcontrolErrorsComponent {
   @Input()
-  message = '';
+  set message(msg: string) {
+    this.elementRef.nativeElement.innerHTML = msg;
+  }
+
+  constructor(private readonly elementRef: ElementRef<HTMLElement>) {}
 }
