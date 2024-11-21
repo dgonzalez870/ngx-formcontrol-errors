e7773ec01f202b90092ec98f257304999df53b35
Dennys Gonzalez <dgonzalez870@yahoo.com>
2024-11-21 07:35:05 -0400

    feat!: angular 19 update

--- projects/ngx-formcontrol-msgs-translate-parser/src/lib/error-msg-translate/error-msg-translate.component.ts
+++ projects/ngx-formcontrol-msgs-translate-parser/src/lib/error-msg-translate/error-msg-translate.component.ts
@@ -11,12 +11,11 @@ import {
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
-  // Use the same name as the default component to avoid changes to the style sheet of existing applications
-  // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'ngx-formcontrol-errors',
-  standalone: true,
-  imports: [TranslateModule],
-  templateUrl: './error-msg-translate.component.html',
+    // Use the same name as the default component to avoid changes to the style sheet of existing applications
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'ngx-formcontrol-errors',
+    imports: [TranslateModule],
+    templateUrl: './error-msg-translate.component.html'
 })
 export class ErrorMsgTranslateComponent implements ErrorMsgComponent {
   @Input()
--- src/app/source-code-section/source-code-section.component.ts
+++ src/app/source-code-section/source-code-section.component.ts
@@ -7,11 +7,10 @@ import {
 import { CodeExample } from '../code-examples/examples-provider';
 
 @Component({
-  selector: 'app-source-code-section',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './source-code-section.component.html',
-  styleUrl: './source-code-section.component.scss',
+    selector: 'app-source-code-section',
+    imports: [CommonModule],
+    templateUrl: './source-code-section.component.html',
+    styleUrl: './source-code-section.component.scss'
 })
 export class SourceCodeSectionComponent {
   @Input()
